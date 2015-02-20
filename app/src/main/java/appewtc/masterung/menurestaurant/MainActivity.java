436531091eb035cb0497c5dc05e94c80387ad6cf
masterUNG <phrombutr@gmail.com>
2015-02-20 16:12:29 +0700

    Alert Dialog

--- app/src/main/java/appewtc/masterung/menurestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/menurestaurant/MainActivity.java
@@ -1,20 +1,43 @@
 package appewtc.masterung.menurestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private UserTABLE objUserTABLE;
     private FoodTABLE objFoodTABLE;
+    private EditText edtUser, edtPassword;
+    private String strUserChoose, strPassowordChoose, strPasswordTrue, strOfficer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial Widget
+        initialWidget();
 
         //Create or Connect SQLite
         createOrConncet();
@@ -22,7 +45,111 @@ public class MainActivity extends ActionBarActivity {
         //Tester Add Value
         //testerAddValue();
 
+        //Delete All data
+        deleteAllData();
+
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
     }   // onCreate
+
+    private void initialWidget() {
+        edtUser = (EditText) findViewById(R.id.edtUser);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+    }   // initialWidget
+
+    public void clickLogin(View view) {
+
+        strUserChoose = edtUser.getText().toString().trim();
+        strPassowordChoose = edtPassword.getText().toString().trim();
+
+        if (strUserChoose.equals("") || strPassowordChoose.equals("") ) {
+            MyAlertDialog objMyAlert = new MyAlertDialog();
+            objMyAlert.alertDialog(MainActivity.this, "Have Space", "Please Fill All EveryBlank");
+        } else {
+            //Check User
+            checkUser();
+        }
+
+    }   // clickLogin
+
+    private void checkUser() {
+
+    }   // checkUser
+
+    private void deleteAllData() {
+        SQLiteDatabase objSQLite = openOrCreateDatabase("restaurant.db", MODE_PRIVATE, null);
+        objSQLite.delete("userTABLE", null, null);
+        objSQLite.delete("foodTABLE", null, null);
+    }   // deleteAllData
+
+    private void synJSONtoSQLite() {
+
+        //Change Policy
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+        }   // if
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        //Create InputStream
+        try {
+
+            HttpClient objHttplient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/poy/get_data_user_poy.php");
+            HttpResponse objHttpResponse = objHttplient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("Restaurant", "InputStream ==> " + e.toString());
+        }
+
+        //Create strJSON
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedReader.readLine()) != null) {
+                objStringBuilder.append(strLine);
+            }   // while
+
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+        } catch (Exception e) {
+            Log.d("Restaurant", "strJSON ==> " + e.toString());
+        }
+
+        //Update userTABLE
+        try {
+
+            final JSONArray objJSONArray = new JSONArray(strJSON);
+            for (int i = 0; i < objJSONArray.length(); i++) {
+
+                JSONObject objJSONObject = objJSONArray.getJSONObject(i);
+                String strUser = objJSONObject.getString("User");
+                String strPassword = objJSONObject.getString("Password");
+                String strOfficer = objJSONObject.getString("Officer");
+                String strAddress = objJSONObject.getString("Address");
+                String strTel = objJSONObject.getString("Tel");
+
+                //Update Data to userTABLE
+                long insertValut = objUserTABLE.addValueToUser(strUser, strPassword, strOfficer, strAddress, strTel);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("Restaurant", "Update SQLite ==> " + e.toString());
+        }
+
+
+
+    }   // synJSONtoSQLite
 
     private void testerAddValue() {
         objUserTABLE.addValueToUser("User", "Password", "Officer", "Address", "Tel");
